7dd39462245eb83e8bc72232bc59ad984b322382
apstefans <arnarpje@gmail.com>
2023-08-01 15:57:10 +0100

    ADD ability to add a task

---
+++ agg_todo.py
@@ -0,0 +1,15 @@
+tasks = []
+
+def add_task():
+    new_task = input("What do you think you'll accomplish? ")
+    tasks.append(new_task)
+    print("I've added that task to your list.")
+    print("Let's see if you'll actually do it you lazy cunt.")
+
+def del_task():
+    pass
+
+def view_tasks():
+    pass
+
+add_task()
