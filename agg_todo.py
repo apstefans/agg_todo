816a2d2073da705d4d697a60206c1d5d9bd1e4ff
apstefans <arnarpje@gmail.com>
2023-08-02 15:37:00 +0100

    Fixed position of program in terminal

--- agg_todo.py
+++ agg_todo.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 # An Aggresive ToDo list
 # The rudest ToDo list you'll ever see
-#test
+
 
 # The list of tasks
 tasks = []
@@ -47,12 +48,14 @@ def add_task():
     new_task = input(MAGENTA + "What do you think you'll accomplish? " + TERMCOLOR)
     print(" ")
     tasks.append(new_task)
+    print("\n" * 100)
     print(GREEN + "I've added", REDBLINK + new_task, TERMCOLOR + GREEN + "to your list.")
     print("Let's see if you'll actually do it you lazy cunt." + TERMCOLOR)
 
 # Delete a task
 def del_task():
     if  len(tasks) == 0:
+        print("\n" * 100)
         print(GREEN + "You first need to decide to actually do something before you decide not to. Stupid." + TERMCOLOR)
     else:
         print(MAGENTA + "These are your current tasks: " + TERMCOLOR)
@@ -65,6 +68,7 @@ def del_task():
             print(" ")
             if choice > 0 and choice <= len(tasks):
                 del tasks[choice-1]
+                print("\n" * 100)
                 print(GREEN + "You give up too easily.")
                 print("Don't try and convince me that you actually")
                 print("completed that task.")
@@ -74,15 +78,17 @@ def del_task():
                 else:
                     print(MAGENTA + "Looks like your task list is empty. You really are lazy.")
                 for i, task in enumerate(tasks):
-                    print(i + 1, BLUE + task)                        
+                    print(i + 1, BLUE + task + TERMCOLOR)                        
             elif choice == 0:
+                print("\n" * 100)
                 print(GREEN + "Indecisive are we?")
             else:
+                print("\n" * 100)
                 print(GREEN + "For fuck sake, pick a task that's there. Try again and don't fail this time!" + TERMCOLOR)
                 print(" ")
                 del_task()
         except:
-            print(" ")
+            print("\n" * 100)
             print(YELLOWBLINK + YELLOW + "THAT'S NOT EVEN A NUMBER! WHAT ARE YOU?! " + TERMCOLOR)
             print(GREEN + "Try again, and since you need a reminder, here is a list of numbers:")
             print(MAGENTA + "0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9")
@@ -93,9 +99,11 @@ def del_task():
 # View the task list
 def view_tasks():
     if len(tasks) == 0:
+        print("\n" * 100)
         print(GREEN + "You don't have any tasks. I knew you were no good." + TERMCOLOR)
         #print(" ")
     else:
+        print("\n" * 100)
         print(MAGENTA + "These are the tasks you've not done yet:" + TERMCOLOR)
         print(" ")
 
@@ -104,12 +112,14 @@ def view_tasks():
 
 # Quit program
 def quit():
+    print("\n" * 100)
     print(GREEN + "Fine. Go then. I didn't like you anyway.")
     print(GREENBLINK + "." + REDBLINK + "." + YELLOWBLINK + "." + BLUE + "." + MAGENTA + "." + GREENBLINK + "." + REDBLINK + "." + YELLOWBLINK + "." + BLUE + "." + MAGENTA + "." + GREENBLINK + "." + REDBLINK + "." + YELLOWBLINK + "." + BLUE + "." + MAGENTA + "." + GREENBLINK + "." + REDBLINK + "." + YELLOWBLINK + "." + BLUE + "." + MAGENTA + "." + GREENBLINK + "." + TERMCOLOR)
     print(YELLOWBLINK + "          .-.\n          |" + BLUE + "U" + YELLOWBLINK + "|\n          | |\n          | |\n         _| |_\n        | | | |-. \n       /|     ` |\n      | |       |\n      |         |\n      \         /\n       |       |\n       |       |\n" + TERMCOLOR)    
     print(MAGENTA + "          Cunt." + TERMCOLOR)
     exit()
 
+print("\n" * 100)
 def main():
     while True:
         start()
